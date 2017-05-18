b46bb00bfeef736a1a90c08856977de81c7b88aa
JarbasAI <somom@30wave.com>
2017-05-18 20:28:24 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -19,11 +19,13 @@ from mycroft.skills.core import MycroftSkill
 from mycroft.util.log import getLogger
 import os
 import json
+import subprocess
 from os.path import dirname
 __author__ = 'chrison999'
 
 logger = getLogger(__name__)
 
+SKILLS_DIR = dirname(dirname(__file__))
 
 class HelpMeSkill(MycroftSkill):
     def __init__(self):
@@ -38,53 +40,77 @@ class HelpMeSkill(MycroftSkill):
         self.build_layers()
         self.build_help()
 
-    def build_help(self, reset = False):
-        SKILLS_DIR = dirname(dirname(__file__))
+    def build_help(self, reset=True):
         # Scan the folder that contains Skills.
         list = filter(lambda x: os.path.isdir(
             os.path.join(SKILLS_DIR, x)), os.listdir(SKILLS_DIR))
-        # see if help file exists
         for skill_folder in list:
+            # generate basic help
+            self.help_files[skill_folder] = self.generate_basic_help(skill_folder)
             path = os.path.join(SKILLS_DIR, skill_folder)
-            # checking if help exists
             if "help.json" not in os.listdir(path) or reset:
-                # create help file
-                help = {}
-                help["skill_name"] = skill_folder.lower().replace("_"," ").replace("-"," ").replace("mycroft","").replace("skill","")
-                help["description"] = "no description provided for this skill"
-                # TODO try to get github repo for website
-                help["support"] = {"email":"not provided", "website":"not provided"}
-                help["commands"] = {}  # command : "description of what command does"
-                # get path for this skills vocab
-                cpath = path + "/vocab/" + self.lang
-                # check if vocab files were defined for this skill
-                if os.path.exists(cpath):
-                    # list of vocab files
-                    list = filter(lambda x: os.path.isfile(
-                        os.path.join(cpath, x)), os.listdir(cpath))
-                    for v in list:
-                        vpath = cpath + "/" + v
-                        # read each command from each vocab file
-                        with open(vpath, 'r') as voc_file:
-                            for line in voc_file.readlines():
-                                parts = line.strip().split("|")
-                                entity = parts[0]
-                                if entity != "" and entity != " ":
-                                    help["commands"].setdefault(entity, "")
-                # TODO get regex commands
-                self.help_files.setdefault(skill_folder, help)
-                # TODO save help.json in skill folder
-                with open(path + "/help.json", 'w') as myfile:
-                    file_content = self.help_files[skill_folder]
-                    help = json.dumps(file_content)
-                    myfile.write(help)
-                    myfile.close()
-
+                # if help not in dir, save generated help
+                self.save_help(skill_folder)
             else:
-                with open(path + "/help.json", 'r') as myfile:
-                    file_content = myfile.read()
-                    help = json.loads(file_content)
-                self.help_files.setdefault(skill_folder, help)
+                # if help.json is in dir read it
+                help = self.read_help(skill_folder)
+                # TODO merge missing fields instead of over-writing
+                self.help_files[skill_folder] = help
+
+            print self.help_files[skill_folder]
+
+    def generate_basic_help(self, skill_folder):
+        # this generates basic help files with all the info it can get from the skill folder
+        path = os.path.join(SKILLS_DIR, skill_folder)
+        help = {}
+        help["skill_name"] = skill_folder.lower().replace("_", " ").replace("-", " ").replace("mycroft",
+                                                                                              "").replace(
+            "skill", "")
+        if help["skill_name"][0] == " ":
+            help["skill_name"] = help["skill_name"][1:]
+        help["description"] = "no description provided for this skill"
+        try:
+            website = subprocess.check_output(["git", "remote", "-v"], cwd=path)
+            website = website.replace("origin\t", "").replace(" (fetch)", "").split("\n")[0]
+        except:
+            website = "not provided"
+        help["support"] = {"email": "not provided", "website": website}
+        help["keywords"] = {}  # keyword : "utterance"
+        help["intents"] = [] #{"intent_name":name, "intent_decription":desceiption, "intent_keywords":[keyword list]}
+        # get path for this skills vocab
+        cpath = path + "/vocab/" + self.lang
+        # check if vocab files were defined for this skill
+        if os.path.exists(cpath):
+            # list of vocab files
+            list = filter(lambda x: os.path.isfile(
+                os.path.join(cpath, x)), os.listdir(cpath))
+            for v in list:
+                vpath = cpath + "/" + v
+                v = v.replace(".voc", "")
+                help["keywords"].setdefault(v, [])
+                # read each command from each vocab file
+                with open(vpath, 'r') as voc_file:
+                    for line in voc_file.readlines():
+                        parts = line.strip().split("|")
+                        entity = parts[0]
+                        if entity != "" and entity != " ":
+                            help["keywords"][v].append(entity)
+            return help
+
+    def read_help(self, skill_folder):
+        # read help.json from this skill folder
+        with open(SKILLS_DIR + "/" + skill_folder + "/help.json", 'r') as myfile:
+            file_content = myfile.read()
+            help = json.loads(file_content)
+        return help
+
+    def save_help(self, skill_folder):
+        # save help.json in memory to skill folder
+        with open(SKILLS_DIR + "/" + skill_folder + "/help.json", 'w') as myfile:
+            file_content = self.help_files[skill_folder]
+            help = json.dumps(file_content)
+            myfile.write(help)
+            myfile.close()
 
     def build_layers(self):
         # activate different intents depending on query
